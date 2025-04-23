fa18694fb7e5ba9db3662d6b6bf77162e74c6838
martix3001 <s221442@student.uek.krakow.pl>
2025-04-23 16:35:57 +0200

    working .exe creation with electron

---
+++ SystemyZintegrowane/main.cjs
@@ -0,0 +1,32 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    }
+  })
+
+  // Wczytaj zbudowaną aplikację (Vite → dist)
+  win.loadFile(path.join(__dirname, 'dist', 'index.html'))
+  win.webContents.openDevTools();
+
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
